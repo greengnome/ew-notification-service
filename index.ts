7eb1272710c65256f9114efa08df1cc1603a951f
kirillgladkov <crosfire2011@gmail.com>
2023-08-12 13:16:18 +0300

    chnage port

--- index.ts
+++ index.ts
@@ -2,9 +2,9 @@ import logger, { LogLevelDesc } from 'loglevel';
 import { startServer } from './src/start';
 
 const isTest = process.env.NODE_ENV === 'test';
-const logLevel: LogLevelDesc = (process.env.LOG_LEVEL as LogLevelDesc) ?? (isTest ? 'warn' : 'info');
+const logLevel: LogLevelDesc =
+    (process.env.LOG_LEVEL as LogLevelDesc) ?? (isTest ? 'warn' : 'info');
 
 logger.setLevel(logLevel);
 
-startServer({ port: '3000' });
-
+startServer({ port: '9899' });
